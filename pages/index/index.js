9465aac3d9d584ded38e8e421cf08bd4008a25e6
jackchen <jackchensky@qq.com>
2018-10-18 12:01:19 +0800

    动态获取城市等变量状态

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,6 +20,14 @@ const QQMapWX = require(
   '../../libs/qqmap-wx-jssdk.js'
   )
 
+const UNPROMPTED = 0
+const UNAUTHORIZED = 1
+const AUTHORIZED = 2
+
+const UNPROMPTED_TIPS = "点击获取当前位置"
+const UNAUTHORIZED_TIPS = "点击开启位置权限"
+const AUTHORIZED_TIPS = ""
+
 Page({
   data: {
     nowTemp: '',
@@ -29,7 +37,8 @@ Page({
     todayTemp:'',
     todayDate:'',
     city:'上海',
-    locationTipsText:'点击获取当前位置'
+    locationAuthType: UNPROMPTED,
+    locationTipsText: UNPROMPTED_TIPS
   },
   onPullDownRefresh(){
     this.getNow(()=>{
@@ -41,7 +50,21 @@ Page({
     this.qqmapsdk = new QQMapWX({
       key: 'EAXBZ-33R3X-AA64F-7FIPQ-BY27J-5UF5B'
     })
-    this.getNow()
+    wx.getSetting({
+      success: res=>{
+        let auth = res.authSetting['scope.userLocation']
+        this.setData({
+          locationAuthType: auth? AUTHORIZED:
+          (auth===false)?UNAUTHORIZED:UNPROMPTED,
+          locationTipsText: auth? AUTHORIZED:
+          (auth===false)?UNAUTHORIZED_TIPS:UNPROMPTED_TIPS
+        })
+        if (auth)
+          this.getCityAndWeather()
+        else
+          this.getNow()
+      }
+    })
   },
   getNow(callback){
     wx.request({
@@ -111,8 +134,16 @@ Page({
   },
 
 onTapLocation(){
+  this.getCityAndWeather()
+},
+
+getCityAndWeather() {
     wx.getLocation({
       success: res=>{
+        this.setData({
+          locationAuthType: AUTHORIZED,
+          locationTipsText: AUTHORIZED_TIPS
+        })
         this.qqmapsdk.reverseGeocoder({
           location: {
             latitude: res.latitude,
@@ -128,6 +159,12 @@ onTapLocation(){
           }
         })
       },
+      fail: () => {
+          this.setData({
+            locationAuthType: UNAUTHORIZED,
+            locationTipsText:UNAUTHORIZED_TIPS
+          })
+      }
     })
   }
 })
